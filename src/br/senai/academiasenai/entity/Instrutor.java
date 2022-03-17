0055483f50a4cc2a50594677535ccf95f80e8af7
Kaylaine Vieira <kaylainevieira3@gmail.com>
2022-03-17 20:06:33 -0300

    Inicio do desenvolvimento dos recursos de gestão de instrutores e equipamentos

---
+++ src/br/senai/academiasenai/entity/Instrutor.java
@@ -0,0 +1,24 @@
+package br.senai.academiasenai.entity;
+
+public class Instrutor {
+
+	private Integer id;
+	
+	private String nomeCompleto;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+}
