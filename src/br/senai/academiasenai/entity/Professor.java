ef6552110b558a83d565731f40ddfd70eaa8eeeb
Kaylaine Vieira <kaylainevieira3@gmail.com>
2022-03-17 21:28:22 -0300

    Adicionado novo atributo

--- src/br/senai/academiasenai/entity/Professor.java
+++ src/br/senai/academiasenai/entity/Professor.java
@@ -1,11 +1,15 @@
 package br.senai.academiasenai.entity;
 
+import java.time.LocalDate;
+
 public class Professor {
 
 	private Integer id;
 	
 	private String nomeCompleto;
 
+	private LocalDate dataDeAdmissao;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -20,5 +24,13 @@ public class Professor {
 
 	public void setNomeCompleto(String nomeCompleto) {
 		this.nomeCompleto = nomeCompleto;
+	}
+
+	public LocalDate getDataDeAdmissao() {
+		return dataDeAdmissao;
+	}
+
+	public void setDataDeAdmissao(LocalDate dataDeAdmissao) {
+		this.dataDeAdmissao = dataDeAdmissao;
 	}
 }
